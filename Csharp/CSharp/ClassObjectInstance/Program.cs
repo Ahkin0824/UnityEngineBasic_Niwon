a7736c7887dd4212009788d0d45fbed756ab9d30
Ahkin0824 <syamsyanin1020@gmail.com>
2022-09-16 19:47:41 +0900

    20220916
    2022-09-16

--- Csharp/CSharp/ClassObjectInstance/Program.cs
+++ Csharp/CSharp/ClassObjectInstance/Program.cs
@@ -8,20 +8,48 @@ namespace ClassObjectInstance
         {
             // new 키워드
             // 동적할당 키워드
+            // 객체화 : 클래스타입의 멤버들만큼의 공간을 할당하는 과정
+            // 객체 : 클래스타입의 멤버들만큼 할당된 메모리 공간
+            // 인스턴트화 : 객체에 실제 데이터를 대입하는 과정
+            // 인스턴트 : 객체는 객체지만, 각 멤버들의 데이터가 초기화되어있는 상태의 객체
+
+            // C# 에서 생성자를 호출하는 과정이 객체화 이면서 인스턴스화 이다.
+            // 인스턴스 ⊂ 객체
             Human human = new Human();
-            int a = 3;
+
+            // .연산자
+            // 멤버 접근 연산자
+            human.height = 150.0f;
+            human.name = "홍길동";
+            human.Breath();
+            Console.WriteLine(human.height);
+            Console.WriteLine($"성별 : {human.genderCharacter}");
+
+            // 객체의 멤버변수는 초기화 값이 없을경우
+            // BSS 영역에 저장되며, 해당영역은 모든 비트가 0으로 세팅되기때문에
+            // 지역변수처럼 반드시 초기화를 할 필요가 없다.
+            
+            //char tempChar;
+            // Console.WriteLine($"성별 : {tempChar}");
         }
     }
 
     // 클래스도 사용자정의 '자료형'
     public class Human
     {
-        int age = 1;
-        float height;
-        double weight;
-        bool isReseting;
-        char genderCharacter;
-        string name;
+        // 보호수준을 결정하는 접근 제한자
+        // public : 접근 제한 없음
+        // private : 해당 객체 외 접근 접근 제한
+        // protected : 해당객체 및 자식객체 외 접근제한
+        // internal : 동인 어셈블리(프로젝트) 외 접근제한
+        // class 의 멤버들에게 접근제한자를 명시 해두지 않을경우
+        // private 가 기본값이 된다.
+        public int age = 1;
+        public float height;
+        public double weight;
+        public bool isReseting;
+        public char genderCharacter;
+        public string name;
 
         // 생성자
         // 객체를 생성하는 함수
@@ -35,17 +63,26 @@ namespace ClassObjectInstance
         // 값 형식 Vs 참조형식
         // 값 형식 : 스택영역에 데이터가 할당됨.
         // ex) int, float, double, char, struct ..
-        // 참조형식 : 힙영역에 데이터가 할당됨.
+        // 참조 형식 : 힙영역에 데이터가 할당됨.
         // ex) class, array, string ..
         public Human()
+        {
+            height = 160.0f;
+            weight = 300.0f;
+        }
+
+        // 소멸자
+        // 해당 객체를 메모리에서 해제할때 호출하는 함수
+        // 가비지컬렉터가 해당 객체가 참조되지 않을경우 호출해주기때문에 직접 호출하는 일은 없다.
+        ~Human()
         {
 
         }
 
-        void Breath()
+        public void Breath()
         {
             Console.WriteLine(name + "(이)가 숨을쉰다");
-            Console.WriteLine($" {name} (이)가 숨을쉰다");
+            Console.WriteLine($"{name} (이)가 숨을쉰다");
         }
     }
 }
---
+++ Csharp/CSharp/Excample01_Classproject/Program.cs
@@ -0,0 +1,111 @@
+﻿using Excample01_Classproject;
+using System;
+using System.Xml.Linq;
+
+// orc 라는 클래스 작성 예제
+// Orc 는
+// 이름, 키, 몸무게, 나이, 성별문자, 쉬고있는지아닌지? 에대한 특징을 가지고있으며(멤버변수)
+// 휘드르기 (Smash), 점프하기(Jump) 라는 기능(멤버함수)를 가지고 있어야한다
+// Orc 클래스를 작성한후
+//
+// Main 함수 내에서 Orc 객체를 두개 만듭니다.
+// 첫번째 객체는
+// 이름 : 상급오크
+// 키 : 240.2
+// 몸무게 : 200
+// 나이 : 149
+// 쉬고있는지? : false
+//
+// 두번째 객체는
+// 이름 : 하급오크
+// 키 : 140.4
+// 모무게 : 120.0
+// 나이 60
+// 성별 문자 : 여
+// 쉬고있는지 : true
+//
+// 2. 각 오크들이 쉬고있는지 체크한후 쉬고있는 오크에게 점프와 휘두르기를 시전시킵니다
+// (멤버함수 호출)
+namespace Excample01_Classproject
+{
+
+    // Naming convention
+    // PascalCase
+    // camelCase
+    // snake_case
+    //
+    // 클래스이름 , 함수이름은 Pascalcase
+    // public 변수 : Pascalcase
+    // privite 변수 : _camelcase
+    // 지역변수 : camelCase
+    // 상수 : UPPER_SNAKE_CASE / SNAKE_CASE
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Orc orc1 = new Orc();
+            orc1.Name = "상급오크";
+            Console.WriteLine($"이름 : {orc1.Name}");
+            orc1.Height = 240.0f;
+            Console.WriteLine($"키 : {orc1.Height}");
+            orc1.Age = 149;
+            Console.WriteLine($"나이 : {orc1.Age}");
+            orc1.GenderCharacter = '남';
+            Console.WriteLine($"성별 : {orc1.GenderCharacter}");
+            orc1.IsReseting = false;
+            Console.WriteLine($"쉬고있는가? : {orc1.IsReseting}");
+
+
+            Orc orc2 = new Orc();
+            orc2.Name = "하급오크";
+            Console.WriteLine($"이름 : {orc2.Name}");
+            orc2.Height = 140.4f;
+            Console.WriteLine($"키 : {orc2.Height}");
+            orc2.Age = 60;
+            Console.WriteLine($"나이 : {orc2.Age}");
+            orc2.GenderCharacter = '여';
+            Console.WriteLine($"성별 : {orc2.GenderCharacter}");
+            orc2.IsReseting = true;
+            Console.WriteLine($"쉬고있는가? : {orc2.IsReseting}");
+
+            if (orc1.IsReseting)
+            {
+                orc1.Smash();
+                orc1.Jump();
+            }
+
+            if (orc2.IsReseting)
+            {
+                orc2.Smash();
+                orc2.Jump();
+            }
+        }
+    }
+
+    public class Orc
+    {
+        private bool _camelcase;
+        public int Age;
+        public float Height;
+        public double weight;
+        public bool IsReseting;
+        public char GenderCharacter;
+        public string Name;
+
+        // 함수
+        public void Smash()
+        {
+            Console.WriteLine(Name + "(이)가 도끼를휘두른다");
+        }
+
+        public void Jump()
+        {
+            Console.WriteLine(Name + "(이)가 힘차게 튀어오른다");
+        }
+
+    }
+
+
+
+
+}
