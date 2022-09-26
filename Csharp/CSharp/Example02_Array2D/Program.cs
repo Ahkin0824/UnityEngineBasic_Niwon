231bb5155978fd78ada51dabb8d41586e908bd71
Ahkin0824 <syamsyanin1020@gmail.com>
2022-09-26 20:00:09 +0900

    CSharp - Array2D Example, Structure
    Array2D Example

--- Csharp/CSharp/Example02_Array2D/Program.cs
+++ Csharp/CSharp/Example02_Array2D/Program.cs
@@ -4,11 +4,18 @@ namespace Example02_Array2D
 {
     internal class Program
     {
-        static int[,] map = new int[5, 5] // [행갯수 , 열갯수]
+        // 3차원배열
+
+        // static int[,,] = new int[3, 4, 5,]
+        
+        // 0 은 갈수 있는 길 , 1은 벽
+
+        // 2 차원 배열
+        static int[,] map = new int[5, 5]
         {
-            { 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 1 },
             { 0, 1, 1, 1, 1 },
-            { 0, 0, 0, 1, 1 },
+            { 0, 0, 0, 1 ,1 },
             { 1, 1, 0, 0, 0 },
             { 0, 1, 1, 0, 0 }
         };
@@ -24,17 +31,17 @@ namespace Example02_Array2D
 
                 switch (input)
                 {
-                    case "MoveLeft":
-                        player.MoveLeft();
+                    case "L":
+                        player.MoveLeft(map);
                         break;
-                    case "MoveRight":
-                        player.MoveRight();
+                    case "R":
+                        player.MoveRight(map);
                         break;
-                    case "MoveDown":
-                        player.MoveDown();
+                    case "D":
+                        player.MoveDown(map);
                         break;
-                    case "MoveUp":
-                        player.MoveUp();
+                    case "U":
+                        player.MoveUp(map);
                         break;
                     default:
                         Console.WriteLine("wrong input");
@@ -56,7 +63,7 @@ namespace Example02_Array2D
                     else if (map[j, i] == 1)
                         Console.Write("■");
                     else if (map[j, i] == 2)
-                        Console.WriteLine("▣");
+                        Console.Write("▣");
                 }
                 Console.WriteLine();
             }
@@ -75,29 +82,91 @@ namespace Example02_Array2D
 
             public void MoveLeft(int[,] map)
             {
-                // 맵 범위를 넘어가는지 체크
+                // 이동하려는 위치가 맵의 경계를 넘어가는지 체크
                 if (_x - 1 < 0)
-                    Console.WriteLine($"플레이어를 왼쪽으로 이동시킬 수 없습니다. 현재위치 : {_x}, {_y}");
-                else if (map[_y, _x - 1] != 0)
-                    Console.WriteLine($"플레이어를 왼쪽으로 이동시킬 수 없습니다. 현재위티 : {_x}, {_y}");
+                {
+                    Console.WriteLine($"플레이어를 왼쪽으로 이동시킬 수 없습니다. (맵의 경계)");
+                }
+                // 이동하려는 위치가 이동가능하면 이동
+                else if (map[_y, _x - 1] == 0)
+                {
+                    map[_y, _x--] = 0;
+                    map[_y, _x] = 2;
+                    Console.WriteLine($"플레이어 왼쪽으로 한칸 이동. 현재 위치 {_x}, {_y}");
+                }
+                // 이동하려는 위치가 벽이면 이동안함
+                else if (map[_y, _x - 1] == 1)
+                {
+                    Console.WriteLine($"플레이어를 왼쪽으로 이동시킬 수 없습니다. (벽)");
+                }
             }
 
-            public void MoveRight()
+            public void MoveRight(int[,] map)
             {
-                map[_y, _x--] = 0;
-                map[_y, _x] = 2;
-                Console.WriteLine($"플레이어 왼쪽으로 한칸 이동함. 현재위치 : {_x}, {_y}");
+                // 이동하려는 위치가 맵의 경계를 넘어가는지 체크
+                if (_x + 1 > map.GetLength(1) - 1)
+                {
+                    Console.WriteLine($"플레이어를 오른쪽으로 이동시킬 수 없습니다. (맵의 경계)");
+                }
+                // 이동하려는 위치가 이동가능하면 이동
+                else if (map[_y, _x + 1] == 0)
+                {
+                    map[_y, _x++] = 0;
+                    map[_y, _x] = 2;
+                   
+                    Console.WriteLine($"플레이어 오른쪽으로 한칸 이동. 현재 위치 {_x}, {_y}");
+                }
+                // 이동하려는 위치가 벽이면 이동안함
+                else if (map[_y, _x + 1] == 1)
+                {
+                    Console.WriteLine($"플레이어를 오른쪽으로 이동시킬 수 없습니다. (벽)");
+                }
             }
 
-            public void MoveDown()
+            public void MoveUp(int[,] map)
             {
-
+                // 이동하려는 위치가 맵의 경계를 넘어가는지 체크
+                if (_y - 1 < 0)
+                {
+                    Console.WriteLine($"플레이어를 위쪽으로 이동시킬 수 없습니다. (맵의 경계)");
+                }
+                // 이동하려는 위치가 이동가능하면 이동
+                else if (map[_y - 1, _x] == 0)
+                {
+                    map[_y--, _x] = 0;
+                    map[_y, _x] = 2;
+                    _y--;
+                    Console.WriteLine($"플레이어 위쪽으로 한칸 이동. 현재 위치 {_x}, {_y}");
+                }
+                // 이동하려는 위치가 벽이면 이동안함
+                else if (map[_y - 1, _x] == 1)
+                {
+                    Console.WriteLine($"플레이어를 위쪽으로 이동시킬 수 없습니다. (벽)");
+                }
             }
 
-            public void MoveUp()
+            public void MoveDown(int[,] map)
             {
-
+                // 이동하려는 위치가 맵의 경계를 넘어가는지 체크
+                if (_y + 1 > map.GetLength(0) - 1)
+                {
+                    Console.WriteLine($"플레이어를 아랫쪽으로 이동시킬 수 없습니다. (맵의 경계)");
+                }
+                // 이동하려는 위치가 이동가능하면 이동
+                else if (map[_y + 1, _x] == 0)
+                {
+                    map[_y++, _x] = 0;
+                    map[_y, _x] = 2;
+                    _y++;
+                    Console.WriteLine($"플레이어 아랫쪽으로 한칸 이동. 현재 위치 {_x}, {_y}");
+                }
+                // 이동하려는 위치가 벽이면 이동안함
+                else if (map[_y + 1, _x] == 1)
+                {
+                    Console.WriteLine($"플레이어를 아랫쪽으로 이동시킬 수 없습니다. (벽)");
+                }
             }
         }
     }
 }
+
---
+++ Csharp/CSharp/Structure/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+// 구조체
+// 사용자 정의 자료형
+// 멤버변수, 멤버함수를 가짐
+// 생성자가 있음.
+/* (단 생성자는 매개변수로 모든맴버 변수에
+    대응되는 파라미터를 가지고 있어야 하며,
+    생성자호출시 모든멤버변수는 초기화 되어야 한다.) */
+// 왜 멤버변수가 초기화 되어야 하냐?
+// 구조체는 값 형식이고 스택영역에 할당되기 때문.
+
+
+// 구조체 VS 클래스
+// 모든 멤버의 크기 합이 16byte를 초과할 경우 클래스를 사용한다.
+// 값의 전달이 빈번하게 일어나는 경우 구조체를 쓴다.
+// ex : 함수에 인자로 전달해야 하는경우
+// 잠깐 썼다가 사라지는 경우가 잦을때 구조체를 쓴다.
+// ex : 클래스 타입 객체 해제시 가비지 컬렉션 부하 증가
+
+// 가비지컬렉션이란?
+// https://www.notion.so/f27a1263326b47eca852eca8ee264b2a?v=7bfefd70637241e0b8dfbff65acb319e&p=abbb2475f4114b89b254c83f1dde2c86&pm=s
+
+namespace Structure
+{
+    public struct Stats
+    {
+        // alt + shift + 마우스 커서 = 다중선택
+        public int STR;
+        public int DEX;
+        public int INT;
+        public int LUK;
+
+        public int CalcCombatPoint ()
+        {
+            return STR + DEX + INT + LUK;
+        }
+    }
+
+    public class Warrior
+    {
+        public Stats Stats;
+    }
+
+    public class Stats_Class
+    {
+        public Stats Stats;
+    }
+
+    public struct WarriorFamily
+    {
+        public Warrior Warrior1; //스택영역에 참조 주소만 가지고있음.
+        public Warrior Warrior2; 
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 스택영역에 할당
+            Stats stats = new Stats()
+            {
+                STR = 100,
+                INT = 200,
+                DEX = 400,
+                LUK = 800
+            };
+
+            Stats_Class stats_Class = new Stats_Class()
+            {
+                STR = 100,
+                INT = 200,
+                DEX = 400,
+                LUK = 800
+            }
+
+            Warrior warrior = new Warrior();
+            // warrior.Stats 는 힙영역에 할당.
+            Console.WriteLine(stats.CalcCombatPoint());
+        }
+    }
+}
